dc64eaf245f6fbc828089777f910f48ebe61a871
gold999-lgtm <72864295+gold999-lgtm@users.noreply.github.com>
2022-01-19 13:55:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ def open_browser():
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-        basepath = os.path.dirname(__file__)
+        
         file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
+            'uploads', secure_filename(f.filename))
         f.save(file_path)
         preds = model_predict(file_path, loaded_model)
         return preds
